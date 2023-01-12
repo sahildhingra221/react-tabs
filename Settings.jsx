aa31d78a75c19421f0db9c8cd6da6e17ede45042
tradestealer999 <sahildhingra221@gmail.com>
2023-01-13 01:29:55 +0530

    --

--- Settings.jsx
+++ Settings.jsx
@@ -22,20 +22,20 @@ const Settings = () => {
         <Static>
             <Heading pt='10px' pb='50px' as='h1' size='lg' fontWeight='500'>Settings</Heading>
             <ul className="tab-nav">
-                <li onClick={() =>setActiveTab(1)} className={activeTab==1 ? "active" : ""}>
+                <li onClick={() =>setActiveTab(1)} className={activeTab===1 ? "active" : ""}>
                     My Details
                 </li>
-                <li onClick={() =>setActiveTab(2)} className={activeTab==2 ? "active" : ""}>
+                <li onClick={() =>setActiveTab(2)} className={activeTab===2 ? "active" : ""}>
                     Themes
                 </li>
-                <li onClick={() =>setActiveTab(3)} className={activeTab==3 ? "active" : ""}>
+                <li onClick={() =>setActiveTab(3)} className={activeTab===3 ? "active" : ""}>
                     Password
                 </li>
             </ul>
             <div className="tab-content">
-                <div className={"tab-content-item "+(activeTab==1 ? "current" : "")}>Themes</div>
-                <div className={"tab-content-item "+(activeTab==2 ? "current" : "")}>Password</div>
-                <div className={"tab-content-item "+(activeTab==3 ? "current" : "")}>My Details</div>
+                <div className={"tab-content-item "+(activeTab===1 ? "current" : "")}>Themes</div>
+                <div className={"tab-content-item "+(activeTab===2 ? "current" : "")}>Password</div>
+                <div className={"tab-content-item "+(activeTab===3 ? "current" : "")}>My Details</div>
             </div>
         </Static>
     </>
